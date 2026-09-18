a6c75278e098e65c281613f2533aabb34417ff56
Hunon <2997898593@qq.com>
2026-09-18 19:46:55 +0800

    Add files via upload

---
+++ SqList/SqList.h
@@ -0,0 +1,20 @@
+#ifndef _SQLIST_H_
+#define _SQLIST_H_
+#include <stdlib.h>
+#include <stdio.h>
+#define MAXNUMBER 200
+#define ERROR 0
+#define OK 1
+#define FALSE 0
+#define TRUE 1
+typedef int Status;
+typedef int ElmType;
+typedef struct
+{
+    ElmType data[MAXNUMBER];
+    int length;
+}SqList;
+Status GetElm(SqList* L,int i,ElmType *e);
+Status ListInsert(SqList* L,int i,ElmType *e);
+Status ListDelete(SqList* L,int i,ElmType* e);
+#endif // !_SQLIST_H_
